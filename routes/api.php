d79f7e7be274032c4aab080d9a6eb5c3c246d4b4
Demilson Pereira <demilson.d2102@gmail.com>
2022-09-24 15:35:43 -0400

    Criado o método de autenticação por cookie

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,7 @@ Route::get('test', function () {
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [AuthController::class, 'user']);
+});
